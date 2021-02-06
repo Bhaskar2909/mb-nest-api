04e4f617549fcd0eff540ca44bfe2804e2f4c90a
Bhaskar <reddybhaskar437@gmail.com>
2021-02-05 18:29:42 -0600

    added files location.entity.ts, user.entity.ts

---
+++ location.entity.ts
@@ -0,0 +1,14 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('Location')
+
+export class Location {
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column()
+    latitude: number; 
+
+    @Column()
+    longitude: number;
+}
